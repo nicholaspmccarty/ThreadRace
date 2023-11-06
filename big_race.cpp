4ec63faf7b9dd061656325a235350724db11622d
mccartnp <nicholasmccarty252@gmail.com>
2023-11-06 11:41:38 -0500

    Add a structure for simplicity, doRace takes the struct param

--- big_race.cpp
+++ big_race.cpp
@@ -5,6 +5,15 @@
 #include <iostream>
 #include <thread>
 
+struct Racer {
+    int numParticipants;
+    int numRaces;
+};
+
+
+void doRace(struct Racer) {
+    // The default value for numRaces is 1
+}
 
 
 
@@ -17,16 +26,13 @@ int main(int argc, char* argv[]) {
         std::cerr << "Usage: " << argv[0] << " <number>" << std::endl;
         return 1;
     }
+    Racer race;
+     int numCars;
+     numCars = std::stoi(argv[1]);
+     int numRaces = (argc == 3) ? std::stoi(argv[2]) : 1;
+     race = {numCars, numRaces};
 
-     int numCars, numRaces;
-     (void) numRaces;
-     if (argc == 2) {
-        numCars = std::stoi(argv[1]);
-        
-     } else {
-        numCars = std::stoi(argv[1]);
-        numRaces = std::stoi(argv[2]);
-     }
+     doRace(race);
 
 
     
